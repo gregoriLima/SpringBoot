19cb989eac05468a2645fdb7cfee7f54f9b01047
gregoriLima <54556576+gregoriLima@users.noreply.github.com>
2020-06-23 11:26:34 -0300

    Added thymeleaf dependency

--- src/main/java/com/example/demo/controller/AppController.java
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -1,17 +1,24 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Controller;
 
 //controller para fazer a exibição da propriedade app.message do application.properties
-@RestController
+//@RestController
+@Controller
 public class AppController {
 	
 	@Value("${app.message}")//faz a injeção de todos os valores com esta annotation com o início 'app.message'
 	private String appMessage;
+	
+	//@Value("${jedi.html}")
+	private String mes;
 	
 	//basta colocar direto o nome da variável
 	@Value("${ENV_DB:NENHUMA}") //mapeando uma variável de ambiente do sistema e definindo um valor padrão para caso ela não seja encontrada
@@ -27,4 +34,11 @@ public class AppController {
 	public String getEnvironmentVariable() {
 		return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
 	}
+	
+	 @RequestMapping("/jedi")
+		public ModelAndView jediPage() {
+		ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("jedi");
+	    return modelAndView;
+		}
 }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class HelloController {
 
 
-	@GetMapping("/hello")
+	//@GetMapping("/hello")
 	public String helloMessage() {
 		return "Hello Spring Boot! Here i am!";
 	}
+	
 	
 }
 
